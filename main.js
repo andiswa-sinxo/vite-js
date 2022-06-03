85653f9edf002a445a99e80bc7517da871dedd2a
andiswa-sinxo <andiswasinxo@gmail.com>
2022-06-03 12:58:21 +0200

    vite-js

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+document.querySelector('#app').innerHTML =   
+`"I 💚 Alpine JS!" <br></br>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
